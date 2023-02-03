7ca24c327f05e63a79a716e512c5cbf662f605ff
SadiqAgayev <sadiqagayev820@gmail.com>
2023-02-03 10:32:36 +0400

    Dashboard

--- src/components/root/App.js
+++ src/components/root/App.js
@@ -1,9 +1,14 @@
+import { Container } from "reactstrap";
 import Navi from "../navi/Navi";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <div>
       <Navi />
+      <Container>
+        <Dashboard />
+      </Container>
     </div>
   );
 }
